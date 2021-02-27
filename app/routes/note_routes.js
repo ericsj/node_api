1368be5d2ec4dbc405e16877e625927415a8fff3
Eric Segala Jovelli <ericsegalajovelli@gmail.com>
2021-02-27 14:22:08 -0300

    send note to database and return its id or an error

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,16 @@
 exports.note_routes = function(app, db) {
   app.post('/notes', (req, res) => {
+    const note = {
+      title: req.body.title,
+      body: req.body.body,
+    };
+    db.collection('notes').insert(note, (err, result) => {
+      if (err) {
+        res.send({'error': 'An error has occured'});
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
     console.log(req.body);
-    res.send('Hello');
   });
 };
