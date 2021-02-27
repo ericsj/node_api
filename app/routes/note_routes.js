a5d9b6b90a1ef8acd060e72bb70b27373b995a77
Eric Segala Jovelli <ericsegalajovelli@gmail.com>
2021-02-27 16:43:19 -0300

    implement GET route
    New methods:
    - requestToObjectId: returns an ObjectID from a request
    - returnGetResult: returns result of an Retrieve operation(GET)

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -12,6 +12,18 @@ function requestToNote(request) {
 };
 
 /**
+ * returns an ObjectID from a request
+ * @param {object} request
+ * @return {object} ObjectID
+ */
+function requestToObjectId(request) {
+  const ObjectID = require('mongodb').ObjectID;
+  const ID = request.params.id;
+  const DETAILS = {'_id': new ObjectID(ID)};
+  return DETAILS;
+}
+
+/**
  * returns result of an Insert operation(POST)
  * @param {object} err - db error
  * @param {object} result - db result
@@ -25,10 +37,29 @@ function returnPostResult(err, result, response) {
   }
 }
 
+/**
+ * returns result of an Retrieve operation(GET)
+ * @param {object} err - db error
+ * @param {object} item - db result
+ * @param {object} response - response request
+ */
+function returnGetResult(err, item, response) {
+  if (err) {
+    response.send({'error': 'An error has occured'});
+  } else {
+    response.send(item);
+  }
+}
+
 exports.note_routes = function(app, db) {
   app.post('/notes', (request, response) => {
     db.collection('notes').insert(requestToNote(request), (err, result) => {
       returnPostResult(err, result, response);
+    });
+  });
+  app.get('/notes/:id', (request, response) => {
+    db.collection('notes').findOne(requestToObjectId(request), (err, item) => {
+      returnGetResult(err, item, response);
     });
   });
 };
