cd1bdf3ca8fd64bfcaefbcbc81e3b42541bfa361
Eric Segala Jovelli <ericsegalajovelli@gmail.com>
2021-02-27 16:03:15 -0300

    refactor POST: divide into small methods
    - requestToNote method: Generates a note object from a request
    - returnPostResult method: returns result of an Insert operation(POST)

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,16 +1,34 @@
+/**
+ * Generates a note object from a request
+ * @param {object} request - request received
+ * @return {object} note - note object that will be sent to database
+ */
+function requestToNote(request) {
+  const note = {
+    title: request.body.title,
+    body: request.body.body,
+  };
+  return note;
+};
+
+/**
+ * returns result of an Insert operation(POST)
+ * @param {object} err - db error
+ * @param {object} result - db result
+ * @param {object} response - response request
+ */
+function returnPostResult(err, result, response) {
+  if (err) {
+    response.send({'error': 'An error has occured'});
+  } else {
+    response.send(result.ops[0]);
+  }
+}
+
 exports.note_routes = function(app, db) {
-  app.post('/notes', (req, res) => {
-    const note = {
-      title: req.body.title,
-      body: req.body.body,
-    };
-    db.collection('notes').insert(note, (err, result) => {
-      if (err) {
-        res.send({'error': 'An error has occured'});
-      } else {
-        res.send(result.ops[0]);
-      }
+  app.post('/notes', (request, response) => {
+    db.collection('notes').insert(requestToNote(request), (err, result) => {
+      returnPostResult(err, result, response);
     });
-    console.log(req.body);
   });
 };
